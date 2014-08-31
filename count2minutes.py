8ab33608a11d6c34a46f04666e88c5133978b2e7
Omar Mendoza <omaryahir@gmail.com>
2014-08-30 23:00:10 -0500

    Add util to count 2 minutes

--- count2minutes.py
+++ count2minutes.py
@@ -1,16 +1,17 @@
 from datetime import datetime, timedelta
 from os import system
-a = datetime.today() + timedelta(minutes=5)
+a = datetime.today() + timedelta(minutes=2)
 continuar = True;
 print "Contando..."
+c = datetime.today()
 while continuar:
     b = datetime.today()
-    if b > a:
+    if b >= a:
         continuar = False
         msg = "Listo, he contado 2 minutos."
         print msg
         system("say "+msg);
-    else:
-        print b
-
+    elif b >= c:
+        print "contando",b,"  --final:",a
+        c = datetime.today() + timedelta(seconds=1)
 
